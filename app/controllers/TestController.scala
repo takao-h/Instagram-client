c02f35c3cd40d4bdd1a0f038e5960b245db873f1
takao-h <hayashi_takao@cyberbuzz.co.jp>
2017-11-21 22:24:25 +0900

    Test access to json node

--- app/controllers/TestController.scala
+++ app/controllers/TestController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.libs.json._
 import play.api.libs.json.{JsNull,Json,JsString,JsValue}
+import play.api.libs.functional.syntax._
 /**
   * Created by ruth on 2017/11/21.
   */
@@ -79,6 +80,69 @@ class TestController {
   case class Resident(name: String, age: Int, role: Option[String])
   case class Place(name: String, location: Location, residents: Seq[Resident])
 
-  
+  implicit val locationWrites = new Writes[Location] {
+    def writes(location: Location) = Json.obj(
+      "lat" -> location.lat,
+      "long" -> location.long
+    )
+  }
+
+  implicit val residentWrites = new Writes[Resident] {
+    def writes(resident: Resident) = Json.obj(
+      "name" -> resident.name,
+      "age" -> resident.age,
+      "role" -> resident.role
+    )
+  }
+
+  implicit val placeWrites = new Writes[Place] {
+    def writes(place: Place) = Json.obj(
+      "name" -> place.name,
+      "location" -> place.location,
+      "residents" -> place.residents)
+  }
+
+  val place = Place(
+    "Watership Down",
+    Location(51.235685, -1.309197),
+    Seq(
+      Resident("Fiver", 4, None),
+      Resident("Bigwig", 6, Some("Owsla"))
+    )
+  )
+
+  val json = Json.toJson(place)
+
+
+  implicit val locationWrites1: Writes[Location] = (
+    (JsPath \ "lat").write[Double] and
+      (JsPath \ "long").write[Double]
+    )(unlift(Location.unapply))
+
+  implicit val residentWrites1: Writes[Resident] = (
+    (JsPath \ "name").write[String] and
+      (JsPath \ "age").write[Int] and
+      (JsPath \ "role").writeNullable[String]
+    )(unlift(Resident.unapply))
+
+  implicit val placeWrites1: Writes[Place] = (
+    (JsPath \ "name").write[String] and
+      (JsPath \ "location").write[Location] and
+      (JsPath \ "residents").write[Seq[Resident]]
+    )(unlift(Place.unapply))
+
+
+  val lat = (json \ "location" \ "lat").get
+  // returns JsNumber(51.235685)
+
+  val names = json \\ "name"
+  // returns Seq(JsString("Watership Down"), JsString("Fiver"), JsString("Bigwig"))
+
+  //arrayにアクセスする　＼hoge で要素を指定して(int)でindexを指定する
+  val bigwig = (json \ "residents")(1)
+  // returns {"name":"Bigwig","age":6,"role":"Owsla"}
+
+  //jsvalueから変換するとき
+  val minifiedString: String = Json.stringify(json)
 
 }
